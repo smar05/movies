c05ba1879eaef067c06c5bbd52962006c380867a
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-03-17 23:07:28 -0500

    Ajustes dinamicos en el front

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+let body = document.querySelector("body");
+let main = document.querySelector("main");
+let h2Subtitulo = document.querySelector("#subtituloBienvenido");
+let a = document.querySelector("a");
+let listp = document.querySelectorAll("p");
+
+let nombre = prompt("Ingrese su nombre");
+
+if (nombre) {
+  h2Subtitulo.innerHTML += ` ${nombre}`;
+} else {
+  h2Subtitulo.innerHTML += ` invitado`;
+}
+
+h2Subtitulo.style.textTransform = "uppercase";
+
+a.style.color = "#E51B3E";
+
+let colocarFondo = confirm("¿Desea colocar un fondo de pantalla?");
+
+if (colocarFondo) {
+  body.classList.toggle("fondo");
+}
+
+for (let i = 0; i < listp.length; i++) {
+  if (i % 2 == 0) {
+    listp[i].classList.toggle("destacadoPar");
+  } else {
+    listp[i].classList.toggle("destacadoImpar");
+  }
+}
+
+main.style.display = "block";
